d496457a313211e8228f7fd3ad25598028db4ba3
rahul-M-sdet <87rhlmishra@gmail.com>
2024-08-02 09:51:40 +0530

    third commit

--- Program/Slice.string.py
+++ Program/Slice.string.py
@@ -4,3 +4,13 @@ s = input("Enter some string to reversed")
 
 reversestring = s[::-1]
 print("The string is:", reversestring)
+
+# reverse string by using while loop
+
+s = "Software"
+output = ''
+i = len(s)-1
+while i >= 0:
+    output = output + s[i]
+    i=i-1
+print(output)
